aadef1f4b94f6c0661a75571fa092d02b6b9bd94
Filip Gulan <gulan.filip@gmail.com>
2018-01-02 23:01:04 +0100

    Added simple measuer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "utility/bioinf_utility.h"
+#include "rrr/RRRSequence.h"
+#include "wavelet/WaveletTree.h"
 
 int main()
 {
-    std::string file = read_fasta_file("tests/sequences/ecoli.fa");
-    auto len = file.length();
+    std::string bact = read_fasta_file("tests/sequences/bact.fa");
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    auto tree = WaveletTree(bact);
+
+    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+    std::cout << "Time difference (ms) = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() <<std::endl;
     return 0;
 }
