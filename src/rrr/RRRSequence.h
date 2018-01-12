dbcb1954dda780cf15053a83b8da837eff023adf
Jure Cular <jure.cular@fer.hr>
2018-01-12 21:24:15 +0100

    Added code documentation

--- src/rrr/RRRSequence.h
+++ src/rrr/RRRSequence.h
@@ -11,38 +11,89 @@
 class RRRSequence
 {
 public:
-
+    /**
+    * Default class constructor, constructs an empty RRR sequence.
+    */
     RRRSequence() = default;
 
+    /**
+    * Default move constructor.
+    */
     RRRSequence(RRRSequence &&) = default;
 
+    /**
+    * Default copy constructor.
+    */
     RRRSequence(RRRSequence const &) = default;
 
+    /**
+    * Default copy assignment.
+    */
     RRRSequence &
     operator=(RRRSequence const &) = default;
 
+    /**
+    * Default move assignment.
+    */
     RRRSequence &
     operator=(RRRSequence &&) = default;
 
+    /**
+     * Class constructor
+     * @param sequence sequence of 1s and 0s
+     * @param blocks_in_superblock  number of blocks in superblock
+     * @param block_size block size
+     * @param table instance of RRR table
+     */
     RRRSequence(block_vector_t const &sequence,
                 uint8_t blocks_in_superblock,
                 block_size_t block_size,
                 RRRTable const &table);
 
+    /**
+     * Class constructor calculates sizes for blocks and
+     * superblocks from the length of sequence
+     * @param sequence sequence of 1s and 0s
+     * @param table instance of RRR table
+     */
     RRRSequence(block_vector_t const &sequence, RRRTable const &table);
 
+    /**
+     * Rank1 query over sequence
+     * @param index index in the containing sequence
+     * @return rank1 at given index
+     */
     class_t
     rank1(size_t index) const;
 
+    /**
+     * Rank0 query over sequence
+     * @param index index in the containing sequence
+     * @return rank0 at given index
+     */
     class_t
     rank0(size_t index) const;
 
+    /**
+     * Select1 query over sequence
+     * @param count number of 1s
+     * @return index at count-th 1 appears
+     */
     size_t
     select1(uint64_t count) const;
 
+    /**
+     * Select0 query over sequence
+     * @param count number of 0s
+     * @return index at count-th 0 appears
+     */
     size_t
     select0(uint64_t count) const;
-
+    /**
+     * Access query over sequence
+     * @param index index in the seuence
+     * @return value at the index
+     */
     char_t
     access(size_t index) const;
 
--- src/utility/bioinf_utility.h
+++ src/utility/bioinf_utility.h
@@ -8,7 +8,16 @@
 #include <string>
 #include "../shared/common.h"
 
+/**
+ * Used to reac FASTA files
+ * @param filename name of the FASTA file
+ * @return contents of the FASTA file
+ */
 std::string read_fasta_file(char const* filename);
+
+/**
+ * Returns current memory used
+ */
 measured_memory_t get_current_memory();
 
 #endif //BIOINFORMATICS_PROJECT_BIOINF_UTILITY_H
