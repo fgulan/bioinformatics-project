f4162e28f74a07a6bc57365db91fa17a2f97e822
jcular <jcular@toshiba-host.home-cj.net>
2018-01-14 19:57:06 +0100

    Added memory reading for linux

--- src/utility/bioinf_utility.h
+++ src/utility/bioinf_utility.h
@@ -17,10 +17,5 @@ std::string read_fasta_file(char const* filename);
 /**
  * Returns current memory used
  */
-#ifdef __APPLE__
 measured_memory_t get_current_memory();
-#elif __linux__
-double get_current_memory();
-#endif
-
 #endif //BIOINFORMATICS_PROJECT_BIOINF_UTILITY_H
