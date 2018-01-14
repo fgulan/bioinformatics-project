dcbb52e5bf3a48bc4e37b8d9ca56c1611b8f3d22
jcular <jcular@toshiba-host.home-cj.net>
2018-01-14 19:54:37 +0100

    Added memory reading for linux

--- src/utility/bioinf_utility.cpp
+++ src/utility/bioinf_utility.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <fstream>
+#include <cstring>
 #include "bioinf_utility.h"
 
 #ifdef __APPLE__
@@ -42,7 +43,33 @@ measured_memory_t get_current_memory()
     }
     return t_info.resident_size;
 #elif __linux
-    return 0;
+    using std::ios_base;
+    using std::ifstream;
+    using std::string;
+
+    // 'file' stat seems to give the most reliable results
+    //
+    ifstream stat_stream("/proc/self/stat",ios_base::in);
+
+    // dummy vars for leading entries in stat that we don't care about
+    //
+    string pid, comm, state, ppid, pgrp, session, tty_nr;
+    string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+    string utime, stime, cutime, cstime, priority, nice;
+    string O, itrealvalue, starttime;
+
+    // the two fields we want
+    //
+    unsigned long vsize;
+
+    stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+                >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+                >> utime >> stime >> cutime >> cstime >> priority >> nice
+                >> O >> itrealvalue >> starttime >> vsize; // don't care about the rest
+
+    stat_stream.close();
+
+    return vsize;
 #else
     return 0;
 #endif
